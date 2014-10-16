00314010b2188708d751282cc51f1b1800411805
Nicholas Street <nicholas.street@gmail.com>
2014-10-17 00:06:37 +0100

    Adding base version that tests cache calls with download tasks and data tasks
    - Calling download task continuously makes requests (inspect via Charles)
    - Calling data tasks makes a request the first time. All subsequent tests don't make requests

--- Cache Test/ViewController.m
+++ Cache Test/ViewController.m
@@ -1,27 +1,51 @@
-//
-//  ViewController.m
-//  Cache Test
-//
-//  Created by Nicholas Street on 16/10/2014.
-//  Copyright (c) 2014 Nicholas Street. All rights reserved.
-//
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<NSURLSessionDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+NSString * const kTestUrl = @"http://cdn.sstatic.net/stackoverflow/img/sprites.png?v=3c6263c3453b";
+
+#pragma mark - Data Task
+
+- (IBAction)testDataTaskTapped:(id)sender {
+    // Call this as many times as you like. Note that only the first request is made in Charles
+    // Subsequent dataTask calls or downloadTask calls will not make HTTP requests
+    [self downloadUsingNSURLSessionDataTask:[NSURL URLWithString:kTestUrl]];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)downloadUsingNSURLSessionDataTask:(NSURL *)url {
+    NSURLSession *session = [self buildSession];
+    NSURLRequest *request = [self buildRequestWithURL:url];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request];
+    [dataTask resume];
 }
 
+#pragma mark - Download Task
+
+- (IBAction)testDownloadTaskTapped:(id)sender {
+    // Call this as many times as you like. Note that the request is made in Charles
+    // Note that if you have previously called the data task, then no request is made
+    [self downloadUsingNSURLSessionDownloadTask:[NSURL URLWithString:kTestUrl]];
+}
+
+- (void)downloadUsingNSURLSessionDownloadTask:(NSURL *)url {
+    NSURLSession *session = [self buildSession];
+    NSURLRequest *request = [self buildRequestWithURL:url];
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request];
+    [downloadTask resume];
+}
+
+-(NSURLSession*)buildSession {
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:self delegateQueue:nil];
+    return session;
+}
+
+-(NSURLRequest*)buildRequestWithURL:(NSURL*)url {
+    return [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60];
+}
 @end
