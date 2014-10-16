b3a0a67dfdfaf300bf4d33ed386b4af234c27455
Nicholas Street <nicholas.street@gmail.com>
2014-10-16 23:28:23 +0100

    Initial Commit

---
+++ Cache Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cache Test
+//
+//  Created by Nicholas Street on 16/10/2014.
+//  Copyright (c) 2014 Nicholas Street. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cache Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cache Test
+//
+//  Created by Nicholas Street on 16/10/2014.
+//  Copyright (c) 2014 Nicholas Street. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
